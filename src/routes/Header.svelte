bf874e40bf9aa08b7322b3599250b3462438c10e
Ratimon <ratixoxo@gmail.com>
2023-07-17 20:17:34 +0700

    Feat (./): Adding tailwind and first component

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,8 +1,15 @@
 <script>
 	import {url} from '$lib/utils/url';
+	import NavBar from '$lib/components/styled/navigation/NavBar.svelte';
 
 </script>
 
 <header>
+
+	<NavBar
+		links={[
+			{href: url(''), title: 'Home'}
+		]}
+	/>
 
 </header>
